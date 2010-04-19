bd5fc2aa02afd7ed08ebf48d66a237e3a1d383ea
Marko Kocic <marko.kocic@gmail.com>
2010-04-19 19:31:41 +0200

    Add simple art

--- strat.lisp
+++ strat.lisp
@@ -5,8 +5,13 @@
 
 (defun main ()
   (sdl:with-init nil
-    (sdl:window 640 480 :bpp 8 :sw t)
-    (sdl:set-caption "SDL Test!" "Sdl testing")
+    (sdl:window 640 480 :bpp 8 :sw t :double-buffer t)
+    (sdl:set-caption "SDL Test!" "Sdl testing") 
+    (sdl:clear-display sdl:*blue*)
+    (let ((tile (sdl:load-image "art/64x74_blue.png")))
+      ;; (sdl:blit-surface tile))
+      (sdl:draw-surface-at tile (sdl:point :x 30 :y 40)))
+    (sdl:update-display)
     (event-loop)))
     
 (defun event-loop ()
